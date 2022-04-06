ca97552658ae1273161615ff55a92de16a2d1165
Jukiker17 <jurosmartin97@gmail.com>
2022-04-06 21:55:22 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import GithubIcon from "../logos/github_smaller.png";
+import LinkedinIcon from "../logos/linkedin_smaller.png";
+import NetlifyIcon from '../logos/netlify.png';
+import GatsbyIcon from '../logos/gatsby.png';
 
 function Footer() {
   return (
-    <div className='footer app-light-menu'>
-
+    <div className="footer app-light-menu">
+      <div className="footer-container">
+        <div className="footer-up">Made by Martin Juroš </div>
+        <div className="footer-down">
+          <div className="footer-lower-left">
+            <a href="https://www.linkedin.com/in/martin-juroš-37b277224/">
+              Linkedin 
+              <img src={LinkedinIcon} alt="" className="footer-img"/>
+            </a>
+          </div>
+          <div className="footer-lower-center">
+            <a href="https://github.com/Jakker17">
+              GitHub   
+              <img src={GithubIcon} alt="" className="footer-img"/>
+            </a>
+          </div>
+          <div className="footer-lower-right">
+            <a href="https://www.netlify.com">
+              Netlify
+            <img src={NetlifyIcon} alt="" className="footer-img"/>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
