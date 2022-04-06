9145e27345f5609d7ad8d2e99804da8939ecabab
Jukiker17 <jurosmartin97@gmail.com>
2022-04-06 12:40:55 +0200

    fixed resizing

--- src/components/pages/info-components/Contact.js
+++ src/components/pages/info-components/Contact.js
@@ -1,41 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Contact.css";
 
 function Contact() {
+  const [smallerWindow, setSmallerWindow] = useState(false);
+
+  const showSmaller = () => {
+    if (window.innerWidth <= 960) {
+      setSmallerWindow(true);
+    } else {
+      setSmallerWindow(false);
+    }
+  };
+
+  useEffect(() => {
+    showSmaller();
+  }, []);
+
+  window.addEventListener("resize", showSmaller);
+
   return (
-    <div className="content">
-      <div className="contact">
-        <div className="text">
-        <div className="block-underline"></div>
-          <h2 style={
-            {marginBottom:10,
-              marginTop: 100}
-          }>Contact</h2>
-          <p>
-            You can contact me via email, my linkedin or github profile.
-            All of the mentioned will be below.
-          </p>
+    <>
+      {!smallerWindow && (
+        <div className="content">
+          <div className="contact">
+            <div className="text">
+              <div className="block-underline"></div>
+              <h2 style={{ marginBottom: 10, marginTop: 100 }}>Contact</h2>
+              <p>
+                You can contact me via email, my linkedin or github profile. All
+                of the mentioned will be below.
+              </p>
+            </div>
+            <div className="contact-links">
+              <ul className="list-content">
+                <li>
+                  <a href="https://github.com/Jakker17" className="link">
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/martin-juroš-37b277224/"
+                    className="link"
+                  >
+                    Linkedin
+                  </a>
+                </li>
+                <li>Email: jurosmartin97@gmail.com</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="contact-links">
-          <ul className="list-content">
-            <li>
-              <a href="https://github.com/Jakker17" className="link">
-                Github
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/martin-juroš-37b277224/"
-                className="link"
-              >
-                Linkedin
-              </a>
-            </li>
-            <li>Email: jurosmartin97@gmail.com</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+      )}
+      {smallerWindow && <div className="contact-smaller">
+            <div className="text">
+              <div className="block-underline-smaller"></div>
+              <h2 style={{ marginBottom: 10, marginTop: 100 }}>Contact</h2>
+              <p>
+                You can contact me via email, my linkedin or github profile. All
+                of the mentioned will be below.
+              </p>
+            </div>
+            <div className="contact-links">
+              <ul className="list-content">
+                <li>
+                  <a href="https://github.com/Jakker17" className="link">
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/martin-juroš-37b277224/"
+                    className="link"
+                  >
+                    Linkedin
+                  </a>
+                </li>
+                <li>Email: jurosmartin97@gmail.com</li>
+              </ul>
+            </div>
+          </div>}
+    </>
   );
 }
 
--- src/components/pages/info-components/Experience.js
+++ src/components/pages/info-components/Experience.js
@@ -1,48 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Experience.css";
 
 function Experience() {
+  const [smallerWindow, setSmallerWindow] = useState(false);
+
+  const showSmaller = () => {
+    if (window.innerWidth <= 960) {
+      setSmallerWindow(true);
+    } else {
+      setSmallerWindow(false);
+    }
+  };
+
+  useEffect(() => {
+    showSmaller();
+  }, []);
+
+  window.addEventListener("resize", showSmaller);
   return (
-    <div className="content">
-      <div className="experience">
-        <div className="experience-header">
-          <h1>Experience</h1>
-          <div className="block-underline"></div>
-        </div>
-        <div className="experience-block">
-          <h2>Skills</h2>
-          <div className="experience-content">
-            <div>
-              <ul className="list-content">
-                <li>English/Czech</li>
-                <li>Software Development</li>
-                <li>Web Development</li>
-                <li>Windows 7/10</li>
-                <li>Basic Linux/Windows Scripting</li>
-                <li>Basic Docker containering</li>
-                <li>Basic to Advanced knowledge of Kafka</li>
-                <li>Fast Learner</li>
-                <li>Databases: Oracle SQL, PostgreSQL</li>
-              </ul>
+    <>
+      {!smallerWindow && (
+        <div className="content">
+          <div className="experience">
+            <div className="experience-header">
+              <h1>Experience</h1>
+              <div className="block-underline"></div>
+            </div>
+            <div className="experience-block">
+              <h2>Skills</h2>
+              <div className="experience-content">
+                <div>
+                  <ul className="list-content">
+                    <li>English/Czech</li>
+                    <li>Software Development</li>
+                    <li>Web Development</li>
+                    <li>Windows 7/10</li>
+                    <li>Basic Linux/Windows Scripting</li>
+                    <li>Basic Docker containering</li>
+                    <li>Basic to Advanced knowledge of Kafka</li>
+                    <li>Fast Learner</li>
+                    <li>Databases: Oracle SQL, PostgreSQL</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="experience-block">
+              <h2>Programming languages</h2>
+              <div className="experience-content">
+                <div>
+                  <ul className="list-content">
+                    <li>Java</li>
+                    <li>Spring Framework</li>
+                    <li>React.js</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="experience-block">
-          <h2>Programming languages</h2>
-          <div className="experience-content">
-            <div>
-              <ul className="list-content">
-                <li>Java</li>
-                <li>Spring Framework</li>
-                <li>React.js</li>
-                <li>HTML</li>
-                <li>CSS</li>
-              </ul>
+      )}
+      {smallerWindow && (
+          <div className="experience-smaller">
+            <div className="experience-header">
+              <h1>Experience</h1>
+              <div className="block-underline-smaller"></div>
+            </div>
+            <div className="experience-block">
+              <h2>Skills</h2>
+              <div className="experience-content">
+                <div>
+                  <ul className="list-content">
+                    <li>English/Czech</li>
+                    <li>Software Development</li>
+                    <li>Web Development</li>
+                    <li>Windows 7/10</li>
+                    <li>Basic Linux/Windows Scripting</li>
+                    <li>Basic Docker containering</li>
+                    <li>Basic to Advanced knowledge of Kafka</li>
+                    <li>Fast Learner</li>
+                    <li>Databases: Oracle SQL, PostgreSQL</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="experience-block">
+              <h2>Programming languages</h2>
+              <div className="experience-content">
+                <div>
+                  <ul className="list-content">
+                    <li>Java</li>
+                    <li>Spring Framework</li>
+                    <li>React.js</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
