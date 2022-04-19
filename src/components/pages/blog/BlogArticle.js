d6cb89354b0a9c9155712ae074d9d45ff5de7d19
Jukiker17 <jurosmartin97@gmail.com>
2022-04-19 22:18:01 +0200

    edit article CSS

--- src/components/pages/blog/BlogArticle.js
+++ src/components/pages/blog/BlogArticle.js
@@ -1,30 +1,34 @@
 import React, { useEffect, useState, useContext } from "react";
 import Axios from "axios";
 import { BlogArticleContext } from "./Blog-Article-context";
-import './BlogArticle.css';
+import "./BlogArticle.css";
 
 function BlogArticle() {
-    const [articleContext,] = useContext(BlogArticleContext);
-    const [article, setArticle] = useState({});
-    const getFromBE = () => {
-        Axios.get(
-          "http://localhost:8080/articles/getById/" + articleContext
-        ).then((response) => {
-          setArticle(response.data);
-        });
-      };
+  const [articleContext] = useContext(BlogArticleContext);
+  const [article, setArticle] = useState({});
+  const getFromBE = () => {
+    Axios.get("http://localhost:8080/articles/getById/" + articleContext).then(
+      (response) => {
+        setArticle(response.data);
+      }
+    );
+  };
 
-    useEffect(() => {
-        getFromBE();
-      }, []);
+  useEffect(() => {
+    getFromBE();
+  }, []);
 
   return (
     <div className="article">
-      <h1>{article.name}</h1>
-      <h3>{article.categoryId}</h3>
-      <p>{article.article}</p>
+      <div className="article-header">
+        <h1>{article.name}</h1>
+      </div>
+      <div className="article-content">
+        <h3>{article.categoryId}</h3>
+        <p>{article.article}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default BlogArticle
+export default BlogArticle;
